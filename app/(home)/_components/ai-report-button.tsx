e454ecbe0107ef5cd9f8fff4d3dc98c7f89d1664
WerllenTrindade <werllentrindade@hotmail.com>
2024-11-10 22:29:46 -0300

    feat: ensure only premium users can generate ai report

--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -14,12 +14,14 @@ import { useState } from "react";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
 import Markdown from "react-markdown";
 import { Loader2Icon } from "lucide-react";
+import Link from "next/link";
 
 interface AiReportButtonProps {
   month: string;
+  hasPremiunPlan: boolean;
 }
 
-const AiReportButton = ({ month }: AiReportButtonProps) => {
+const AiReportButton = ({ month, hasPremiunPlan }: AiReportButtonProps) => {
   const [report, setReport] = useState<string | null>();
   const [reportIsLoading, setReportIsLoading] = useState(false);
 
@@ -47,28 +49,52 @@ const AiReportButton = ({ month }: AiReportButtonProps) => {
         <Button variant="ghost">Relatório AI</Button>
       </DialogTrigger>
       <DialogContent className="max-w-[600]">
-        <DialogHeader>
-          <DialogTitle>Relatório IA</DialogTitle>
-          <DialogDescription>
-            Use interligência artificial para gerar um relatório com insights
-            sobre suas finanças.
-          </DialogDescription>
-        </DialogHeader>
-        <ScrollArea className="text-withe prose-h3-text-white prose max-h-[450px] prose-h4:text-white prose-strong:text-white">
-          <Markdown>{report}</Markdown>
-        </ScrollArea>
-        <DialogFooter>
-          <DialogClose>
-            <Button variant="ghost">Cancelar</Button>
-          </DialogClose>
-          <Button
-            disabled={reportIsLoading}
-            onClick={handleGenerateReportClick}
-          >
-            {reportIsLoading && <Loader2Icon className="animate-spin" />}
-            Gerar relatório
-          </Button>
-        </DialogFooter>
+        {hasPremiunPlan ? (
+          <>
+            <DialogHeader>
+              <DialogTitle>Relatório IA</DialogTitle>
+              <DialogDescription>
+                Use interligência artificial para gerar um relatório com
+                insights sobre suas finanças.
+              </DialogDescription>
+            </DialogHeader>
+            <ScrollArea className="text-withe prose-h3-text-white prose max-h-[450px] prose-h4:text-white prose-strong:text-white">
+              <Markdown>{report}</Markdown>
+            </ScrollArea>
+            <DialogFooter>
+              <DialogClose>
+                <Button variant="ghost">Cancelar</Button>
+              </DialogClose>
+              <Button
+                disabled={reportIsLoading}
+                onClick={handleGenerateReportClick}
+              >
+                {reportIsLoading && <Loader2Icon className="animate-spin" />}
+                Gerar relatório
+              </Button>
+            </DialogFooter>
+          </>
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>Relatório IA</DialogTitle>
+              <DialogDescription>
+                Você precisa de um plano premium par gerar os relatorios com IA
+              </DialogDescription>
+            </DialogHeader>
+            <ScrollArea className="text-withe prose-h3-text-white prose max-h-[450px] prose-h4:text-white prose-strong:text-white">
+              <Markdown>{report}</Markdown>
+            </ScrollArea>
+            <DialogFooter>
+              <DialogClose>
+                <Button variant="ghost">Cancelar</Button>
+              </DialogClose>
+              <Button asChild>
+                <Link href="/subscriptions">Assinar plano premium</Link>
+              </Button>
+            </DialogFooter>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
